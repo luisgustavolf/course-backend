bcb1a4604597d6d26e6218e4a1318c7fcdd466cd
Luis Gustavo <luisgustavolf@gmail.com>
2023-06-08 11:21:41 -0300

    primeiro setup

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { SigninDto } from './dto/signin.input';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('auth')
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signin')
+  async signin(@Body() body: SigninDto) {
+    return this.authService.login(body)
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('authed')
+  async authed() {
+    return { status: 'ok' }
+  }
+
+  @Get('unauthed')
+  async unauthed() {
+    return { status: 'ok' }
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Course examples')
+    .setDescription('The course API description')
+    .setVersion('1.0')
+    .addTag('course')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000);
+}
+
+bootstrap();
